775fc703d3d947421574f47e055eae546f6071b4
Armando Picón <picondev@gmail.com>
2024-11-18 00:54:30 -0300

    increase targetSdk and compileSdk values to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "la.devpicon.android.githubactions"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "la.devpicon.android.githubactions"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
